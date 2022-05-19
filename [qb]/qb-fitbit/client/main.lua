ce7a06f77d43e07006d0499ca59921858318e3e4
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2022-05-19 17:28:12 -0500

    Updates

--- [qb]/qb-fitbit/client/main.lua
+++ [qb]/qb-fitbit/client/main.lua
@@ -1,4 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local PlayerData = QBCore.Functions.GetPlayerData()
+local isLoggedIn = LocalPlayer.state.isLoggedIn
+local hasFitbit = false
 
 -- Functions
 
@@ -31,6 +34,23 @@ RegisterNetEvent('qb-fitbit:use', function(_, cb)
     cb('ok')
 end)
 
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+    PlayerData = QBCore.Functions.GetPlayerData()
+    hasFitbit = QBCore.Functions.HasItem('fitbit')
+    isLoggedIn = true
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    isLoggedIn = false
+    hasFitbit = false
+    PlayerData = {}
+end)
+
+RegisterNetEvent('QBCore:Client:SetPlayerData', function(val)
+    PlayerData = val
+    hasFitbit = QBCore.Functions.HasItem('fitbit')
+end)
+
 -- NUI Callbacks
 
 RegisterNUICallback('setFoodWarning', function(data, cb)
@@ -51,26 +71,23 @@ end)
 
 CreateThread(function()
     while true do
-        Wait(5 * 60 * 1000)
-        if LocalPlayer.state.isLoggedIn then
-            QBCore.Functions.TriggerCallback('qb-fitbit:server:HasFitbit', function(hasItem)
-                if hasItem then
-                    local PlayerData = QBCore.Functions.GetPlayerData()
-                    if PlayerData.metadata["fitbit"].food ~= nil then
-                        if PlayerData.metadata["hunger"] < PlayerData.metadata["fitbit"].food then
-                            TriggerEvent("chatMessage", Lang:t('info.fitbit'), "warning", Lang:t('warning.hunger_warning', {hunger = round(PlayerData.metadata["hunger"], 2)}))
-                            PlaySound(-1, "Event_Start_Text", "GTAO_FM_Events_Soundset", 0, 0, 1)
-                        end
-                    end
-
-                    if PlayerData.metadata["fitbit"].thirst ~= nil then
-                        if PlayerData.metadata["thirst"] < PlayerData.metadata["fitbit"].thirst then
-                            TriggerEvent("chatMessage", Lang:t('info.fitbit'), "warning", Lang:t('warning.thirst_warning', {thirst = round(PlayerData.metadata["thirst"], 2)}))
-                            PlaySound(-1, "Event_Start_Text", "GTAO_FM_Events_Soundset", 0, 0, 1)
-                        end
+        if isLoggedIn then
+            if hasFitbit then
+                if PlayerData.metadata["fitbit"].food then
+                    if PlayerData.metadata["hunger"] < PlayerData.metadata["fitbit"].food then
+                        TriggerEvent("chatMessage", Lang:t('info.fitbit'), "warning", Lang:t('warning.hunger_warning', {hunger = round(PlayerData.metadata["hunger"], 2)}))
+                        PlaySound(-1, "Event_Start_Text", "GTAO_FM_Events_Soundset", 0, 0, 1)
                     end
                 end
-            end, "fitbit")
+
+                if PlayerData.metadata["fitbit"].thirst then
+                    if PlayerData.metadata["thirst"] < PlayerData.metadata["fitbit"].thirst then
+                        TriggerEvent("chatMessage", Lang:t('info.fitbit'), "warning", Lang:t('warning.thirst_warning', {thirst = round(PlayerData.metadata["thirst"], 2)}))
+                        PlaySound(-1, "Event_Start_Text", "GTAO_FM_Events_Soundset", 0, 0, 1)
+                    end
+                end
+            end
         end
+        Wait(5 * 60 * 1000)
     end
 end)
--- [qb]/qb-fitbit/server/main.lua
+++ [qb]/qb-fitbit/server/main.lua
@@ -1,12 +1,14 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateUseableItem("fitbit", function(source, _)
+QBCore.Functions.CreateUseableItem("fitbit", function(source)
     TriggerClientEvent('qb-fitbit:use', source)
 end)
 
 RegisterNetEvent('qb-fitbit:server:setValue', function(type, value)
     local src = source
     local ply = QBCore.Functions.GetPlayer(src)
+    if not ply then return end
+
     local fitbitData = {}
 
     if type == "thirst" then
@@ -24,15 +26,4 @@ RegisterNetEvent('qb-fitbit:server:setValue', function(type, value)
     end
 
     ply.Functions.SetMetaData('fitbit', fitbitData)
-end)
-
-QBCore.Functions.CreateCallback('qb-fitbit:server:HasFitbit', function(source, cb)
-    local Ply = QBCore.Functions.GetPlayer(source)
-    local Fitbit = Ply.Functions.GetItemByName("fitbit")
-
-    if Fitbit ~= nil then
-        cb(true)
-    else
-        cb(false)
-    end
 end)
