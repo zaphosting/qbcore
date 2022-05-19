f9042fe328e29bbaa0f1b165d6be956a8ac175a8
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2022-05-19 01:27:49 -0500

    Add files via upload

---
+++ [qb]/qb-printer/client/main.lua
@@ -0,0 +1,55 @@
+-- Events
+
+RegisterNetEvent('qb-printer:client:UseDocument', function(ItemData)
+    local DocumentUrl = ItemData.info.url ~= nil and ItemData.info.url or false
+    SendNUIMessage({
+        action = "open",
+        url = DocumentUrl
+    })
+    SetNuiFocus(true, false)
+end)
+
+RegisterNetEvent('qb-printer:client:SpawnPrinter', function()
+    local playerPed = PlayerPedId()
+    local coords    = GetEntityCoords(playerPed)
+    local forward   = GetEntityForwardVector(playerPed)
+    local x, y, z   = table.unpack(coords + forward * 1.0)
+
+    local model = `prop_printer_01`
+    RequestModel(model)
+    while (not HasModelLoaded(model)) do
+        Wait(1)
+    end
+    local obj = CreateObject(model, x, y, z, true, false, true)
+    PlaceObjectOnGroundProperly(obj)
+    SetModelAsNoLongerNeeded(model)
+    SetEntityAsMissionEntity(obj)
+end)
+
+-- NUI
+
+RegisterNUICallback('SaveDocument', function(data, cb)
+    if data.url then
+        TriggerServerEvent('qb-printer:server:SaveDocument', data.url)
+    end
+    cb('ok')
+end)
+
+RegisterNUICallback('CloseDocument', function(_, cb)
+    SetNuiFocus(false, false)
+    cb('ok')
+end)
+
+-- Command
+
+RegisterCommand('useprinter', function()
+    local ped = PlayerPedId()
+    local pos = GetEntityCoords(ped)
+    local PrinterObject = GetClosestObjectOfType(pos.x, pos.y, pos.z, 1.5, `prop_printer_01`, false, false, false)
+    if PrinterObject ~= 0 then
+        SendNUIMessage({
+            action = "start"
+        })
+        SetNuiFocus(true, true)
+    end
+end)
---
+++ [qb]/qb-printer/server/main.lua
@@ -0,0 +1,35 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+local ValidExtensions = {
+    [".png"] = true,
+    [".gif"] = true,
+    [".jpg"] = true,
+    ["jpeg"] = true
+}
+
+local ValidExtensionsText = '.png, .gif, .jpg, .jpeg'
+
+QBCore.Functions.CreateUseableItem("printerdocument", function(source, item)
+    TriggerClientEvent('qb-printer:client:UseDocument', source, item)
+end)
+
+QBCore.Commands.Add("spawnprinter", "Spawn a printer", {}, true, function(source, _)
+	TriggerClientEvent('qb-printer:client:SpawnPrinter', source)
+end, "admin")
+
+RegisterNetEvent('qb-printer:server:SaveDocument', function(url)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local info = {}
+    local extension = string.sub(url, -4)
+    local validexts = ValidExtensions
+    if url ~= nil then
+        if validexts[extension] then
+            info.url = url
+            Player.Functions.AddItem('printerdocument', 1, nil, info)
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['printerdocument'], "add")
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'Thats not a valid extension, only '..ValidExtensionsText..' extension links are allowed.', "error")
+        end
+    end
+end)
